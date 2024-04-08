f3222f889a1501904bef017b8fca7ad53eb82e4c
John Turner <jturner.usa@gmail.com>
2024-04-07 22:38:20 -0400

    use nasm pre-processor to include syscalls and macros from external files

--- main.asm
+++ main.asm
@@ -1,24 +1,11 @@
-%define SYS_WRITE 1
-%define SYS_EXIT 60
-%define STDOUT 1
-%define NEWLINE 0xa
-
-%macro PROLOGUE 0
-        push rbp
-        mov rbp, rsp
-%endmacro
-
-%macro EPILOGUE 0
-        mov rsp, rbp
-        pop rbp
-        ret
-%endmacro
+%include "prologue.asm"
+%include "syscalls.asm"
 
 global _start
         
 _start:
         mov rbp, rsp
 .exit:
-        mov rax, SYS_EXIT
+        mov rax, __NR_exit
         syscall
 
---
+++ prologue.asm
@@ -0,0 +1,10 @@
+%macro PROLOGUE 0
+        push rbp
+        mov rbp, rsp
+%endmacro
+
+%macro EPILOGUE 0
+        mov rsp, rbp
+        pop rbp
+        ret
+%endmacro
